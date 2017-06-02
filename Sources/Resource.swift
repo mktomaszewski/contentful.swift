ae381fc5e2cda02169a4d6eb3cf44c7c5cbdacd0
JP Wright <jp@contentful.com>
2017-06-02 03:20:59 +0200

    Implement fallback locale logic for multi-locale formatted API responses

--- Sources/Resource.swift
+++ Sources/Resource.swift
@@ -34,14 +34,75 @@ public class Resource: ImmutableMappable {
 
 class DeletedResource: Resource {}
 
+// TODO: Document
+public class LocalizableResource: Resource, LocalizedResource {
+
+    /// Currently selected locale
+    public var currentlySelectedLocale: Locale
+
+    /**
+     Content fields. If there is no value for a field associated with the currently selectedLocale,
+     the SDK will walk down fallback chain until a value is found. If there is still no value after
+     walking the full chain, the field will be omitted from
+    */
+    public var fields: [FieldName: Any] {
+        return Localization.fields(forLocale: currentlySelectedLocale, localizableFields: localizableFields, localizationContext: localizationContext)
+    }
+
+    /// TODO: Document that bool returns successful switch of locale.
+    @discardableResult public func setLocale(withCode code: LocaleCode) -> Bool {
+        guard let newLocale =  localizationContext.locales[code] else {
+            return false
+        }
+        currentlySelectedLocale = newLocale
+        return true
+    }
+
+    // Locale to Field mapping.
+    internal var localizableFields: [FieldName: [LocaleCode: Any]]
+
+    // Context used for handling locales during decoding of `Asset` and `Entry` instances.
+    internal let localizationContext: LocalizationContext
+
+
+    // MARK: <ImmutableMappable>
+
+    public required init(map: Map) throws {
+
+        // Optional propery, not returned when hitting `/sync`.
+        var localeCodeSelectedAtAPILevel: LocaleCode?
+        localeCodeSelectedAtAPILevel <- map["sys.locale"]
+
+        guard let localizationContext = map.context as? LocalizationContext else {
+            // Should never get here; but just in case, let's inform the user what the deal is.
+            throw SDKError.localeHandlingError(message: "SDK failed to find the necessary LocalizationContext"
+            + "necessary to properly map API responses to internal format.")
+        }
+
+        self.localizationContext = localizationContext
+
+        // Get currently selected locale.
+        if let localeCode = localeCodeSelectedAtAPILevel, let locale = localizationContext.locales[localeCode] {
+            self.currentlySelectedLocale = locale
+        } else {
+            self.currentlySelectedLocale = localizationContext.default
+        }
+
+        self.localizableFields = try Localization.normalizeLocalizedFieldsToDictionary(map: map, selectedLocale: currentlySelectedLocale)
+
+        try super.init(map: map)
+    }
+}
+
 protocol LocalizedResource {
 
-    // Should this be moved to Resource?
-    var fields: [String: Any]! { get }
+    var fields: [FieldName: Any] { get }
 
-    var locale: String? { get set }
-    var localizedFields: [String: [String: Any]] { get }
-    var defaultLocale: String { get }
+    var currentlySelectedLocale: Locale { get set }
+
+    var localizableFields: [FieldName: [LocaleCode: Any]] { get }
+
+    var localizationContext: LocalizationContext { get }
 }
 
 extension LocalizedResource {
@@ -67,16 +128,6 @@ func +=<K: Hashable, V> (left: [K: V], right: [K: V]) -> [K: V] {
 
 func +<K: Hashable, V> (left: [K: V], right: [K: V]) -> [K: V] {
     return left += right
-}
-
-func fields(_ localizedFields: [String: [String: Any]], forLocale locale: String?, defaultLocale: String) -> [String: Any] {
-    let localeFields = localizedFields[defaultLocale] ?? [String: Any]()
-
-    if let locale = locale, let fields = localizedFields[locale], locale != defaultLocale {
-        return localeFields + fields
-    } else {
-        return localeFields
-    }
 }
 
 public extension Dictionary where Key: ExpressibleByStringLiteral {
